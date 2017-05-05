aec38d938770b21eec77744930f1fa3953a4c415
Brittney <jibarmahamoud@Vincents-MacBook-Pro.local>
2017-05-04 23:16:15 -0400

    OkHttp

--- app/src/main/java/com/jibarapps/stormy/MainActivity.java
+++ app/src/main/java/com/jibarapps/stormy/MainActivity.java
@@ -1,13 +1,50 @@
 package com.jibarapps.stormy;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public  static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "6481c48654f1bd60b1b3a60f2bd20190";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request =  new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()){
+                Log.v(TAG, response.body().string());
+            }
+
+        } catch (IOException e) {
+           Log.e(TAG, "Exception Caught:  ",  e);
+        }
+
+
     }
 }
